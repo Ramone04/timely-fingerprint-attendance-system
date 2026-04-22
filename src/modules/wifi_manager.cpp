0d191a8acb164b461ad5b45258bca7d86c112fda
Ramone04 <rubengarridoscot@gmail.com>
2026-04-22 13:17:30 +0100

    Wifi logic update #2
    DNS config

--- src/modules/wifi_manager.cpp
+++ src/modules/wifi_manager.cpp
@@ -3,14 +3,21 @@
 #include <display_manager.h>
 #include <WiFi.h>
 
-void connectWiFi() {
-    if (WiFi.status() == WL_CONNECTED) return;
-    
+void connectWiFi()
+{
+    if (WiFi.status() == WL_CONNECTED)
+        return;
+
     Serial.printf("A ligar ao WiFi: %s", WIFI_SSID);
     LCDMessage("Connecting to", WIFI_SSID);
     WiFi.mode(WIFI_STA);
+    IPAddress dns1(8, 8, 8, 8);
+    IPAddress dns2(1, 1, 1, 1);
+
+    WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, dns1, dns2);
     WiFi.begin(WIFI_SSID, WIFI_PASS);
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED)
+    {
         delay(500);
         Serial.print(".");
     }
